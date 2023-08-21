3bbcbf419ce32d7932318bf099eb7e10328c7caa
yanyunfan <961241246@qq.com>
2023-08-22 01:51:41 +0800

    perf: mock模块调整和api同名称

--- mock/user.ts
+++ mock/user.ts
@@ -1,7 +1,25 @@
+// 根据角色动态生成路由
 import { MockMethod } from "vite-plugin-mock";
 
-// 模拟刷新token接口
 export default [
+  {
+    url: "/login",
+    method: "post",
+    response: () => {
+      return {
+        code: "00000",
+        msg: "success",
+        data: {
+          username: "admin",
+          // 一个用户可能有多个角色
+          roles: ["admin"],
+          accessToken: "eyJhbGciOiJIUzUxMiJ9.admin",
+          refreshToken: "eyJhbGciOiJIUzUxMiJ9.adminRefresh",
+          expires: "2023/10/30 00:00:00"
+        }
+      };
+    }
+  },
   {
     url: "/refreshToken",
     method: "post",
