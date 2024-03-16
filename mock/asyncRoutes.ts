b92fdc61c6a8162bd45dde25a090f2e8467bd62e
yanyunfan <961241246@qq.com>
2024-03-17 02:01:22 +0800

    feat: 升级5.1.0

---
+++ mock/asyncRoutes.ts
@@ -0,0 +1,146 @@
+// 模拟后端动态生成路由
+import { defineFakeRoute } from "vite-plugin-fake-server/client";
+
+/**
+ * roles：页面级别权限，这里模拟二种 "admin"、"common"
+ * admin：管理员角色
+ * common：普通角色
+ */
+
+const systemRouter = {
+  path: "/system",
+  meta: {
+    icon: "ri:settings-3-line",
+    title: "系统管理",
+    rank: 10
+  },
+  children: [
+    {
+      path: "/system/user/index",
+      name: "SystemUser",
+      meta: {
+        icon: "ri:admin-line",
+        title: "用户管理",
+        roles: ["admin"]
+      }
+    },
+    {
+      path: "/system/role/index",
+      name: "SystemRole",
+      meta: {
+        icon: "ri:admin-fill",
+        title: "角色管理",
+        roles: ["admin"]
+      }
+    },
+    {
+      path: "/system/menu/index",
+      name: "SystemMenu",
+      meta: {
+        icon: "ep:menu",
+        title: "菜单管理",
+        roles: ["admin"]
+      }
+    },
+    {
+      path: "/system/dept/index",
+      name: "SystemDept",
+      meta: {
+        icon: "ri:git-branch-line",
+        title: "部门管理",
+        roles: ["admin"]
+      }
+    }
+  ]
+};
+
+const permissionRouter = {
+  path: "/permission",
+  meta: {
+    title: "权限管理",
+    icon: "ep:lollipop",
+    rank: 12
+  },
+  children: [
+    {
+      path: "/permission/page/index",
+      name: "PermissionPage",
+      meta: {
+        title: "页面权限",
+        roles: ["admin", "common"]
+      }
+    },
+    {
+      path: "/permission/button/index",
+      name: "PermissionButton",
+      meta: {
+        title: "按钮权限",
+        roles: ["admin", "common"],
+        auths: [
+          "permission:btn:add",
+          "permission:btn:edit",
+          "permission:btn:delete"
+        ]
+      }
+    }
+  ]
+};
+
+const frameRouter = {
+  path: "/iframe",
+  meta: {
+    icon: "ep:monitor",
+    title: "外部页面",
+    rank: 10
+  },
+  children: [
+    {
+      path: "/iframe/external",
+      meta: {
+        title: "文档外链"
+      },
+      children: [
+        {
+          path: "/external",
+          name: "https://yiming_chang.gitee.io/pure-admin-doc",
+          meta: {
+            title: "pure-admin-doc",
+            roles: ["admin", "common"]
+          }
+        }
+      ]
+    },
+    {
+      path: "/iframe/embedded",
+      meta: {
+        title: "文档内嵌"
+      },
+      children: [
+        {
+          path: "/iframe/ep",
+          name: "FrameEp",
+          meta: {
+            title: "element-plus",
+            frameSrc: "https://element-plus.org/zh-CN/",
+            keepAlive: true,
+            roles: ["admin", "common"]
+          }
+        }
+      ]
+    }
+  ]
+};
+
+export default defineFakeRoute([
+  {
+    url: "/get-async-routes",
+    method: "get",
+    response: () => {
+      return {
+        code: "00000",
+        msg: "success",
+        data: [systemRouter, permissionRouter, frameRouter]
+      };
+    }
+  }
+]);
